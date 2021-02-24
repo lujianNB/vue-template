15971924a26e38344af5da538c703ebf3bd846f9
lujian <lujiannb@qq.com>
2021-02-24 14:16:47 +0800

    feat: 路由修改

--- src/router/router.js
+++ src/router/router.js
@@ -3,7 +3,7 @@
  * @LastEditors: 卢建
  * @Description: 路由表
  * @Date: 2020-11-19 14:01:23
- * @LastEditTime: 2020-11-30 10:12:22
+ * @LastEditTime: 2021-02-24 14:13:32
  */
 import Home from '../views/Home.vue'
 import Layout from '@/layout'
@@ -38,7 +38,7 @@ export const routes = [
     },
     {
         path: '*',// 必须放在配置最下面 - 错误路由配置
-        component: () => import(/* webpackChunkName: "about" */ '../views/error'),
+        component: () => import(/* webpackChunkName: "error" */ '../views/error'),
         meta: {
             pathName: '错误页'
         }
