cf6c084315d015fffab2363ec7db682556e565d4
lujian <lujiannb@qq.com>
2022-04-18 13:33:01 +0800

    feat: 正规化

--- src/router/router.js
+++ src/router/router.js
@@ -3,13 +3,12 @@
  * @LastEditors: 卢建
  * @Description: 路由表
  * @Date: 2020-11-19 14:01:23
- * @LastEditTime: 2021-02-24 18:16:26
+ * @LastEditTime: 2022-04-18 13:28:11
  */
-import Home from '../views/Home.vue'
+import Home from '../views/home'
 import Layout from '@/layout'
 
-export const routes = [
-    {
+export const routes = [{
         path: '/',
         name: 'Layout',
         component: Layout,
@@ -17,21 +16,19 @@ export const routes = [
         meta: {
             pathName: '基础布局'
         },
-        children: [
-            {
-                path: '/home',
-                name: 'Home',
-                component: Home,
-                meta: {
-                    pathName: '首页',
-                    id: '0'
-                }
-            },
-        ]
+        children: [{
+            path: '/home',
+            name: 'Home',
+            component: Home,
+            meta: {
+                pathName: '首页',
+                id: '0'
+            }
+        }, ]
     },
     {
-        path: '*',// 必须放在配置最下面 - 错误路由配置
-        component: () => import(/* webpackChunkName: "error" */ '../views/error'),
+        path: '*', // 必须放在配置最下面 - 错误路由配置
+        component: () => import( /* webpackChunkName: "error" */ '../views/error'),
         meta: {
             pathName: '错误页'
         }
